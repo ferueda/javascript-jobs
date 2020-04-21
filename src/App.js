98f9363550c15b255d5123acb047e882b65fa583
ferueda <fe.rueda@outlook.com>
2020-04-21 19:14:31 -0400

    did the first MVP of the job container and job card

--- src/App.js
+++ src/App.js
@@ -1,7 +1,65 @@
 import React from 'react';
+import { jobs } from './jobs';
+import styled from 'styled-components';
+
+const JobContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: lightgray;
+`;
+
+const CompanyThumbContainer = styled.div`
+  max-width: 50px;
+`;
+
+const CompanyThumbImg = styled.img`
+  width: 100%;
+`;
+
+const Job = ({ thumb, company, jobTitle, location }) => {
+  return (
+    <JobContainer>
+      <CompanyThumbContainer>
+        <CompanyThumbImg src={thumb} alt={`${company} logo`} />
+      </CompanyThumbContainer>
+      <div>
+        <h3>{company}</h3>
+        <h2>{jobTitle}</h2>
+        <h4>{location}</h4>
+      </div>
+      <div>
+        <button>Apply</button>
+      </div>
+    </JobContainer>
+  );
+};
+
+const JobList = ({ jobs }) => {
+  return (
+    <div>
+      {jobs.map((job) => {
+        return (
+          <Job
+            key={job.id}
+            thumb={job.thumb}
+            company={job.company}
+            jobTitle={job.jobTitle}
+            location={job.location}
+          />
+        );
+      })}
+    </div>
+  );
+};
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Today Jobs</h1>
+      <JobList jobs={jobs} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/jobs.js
@@ -0,0 +1,122 @@
+export const jobs = [
+  {
+    id: 1,
+    company: 'Forethought',
+    jobTitle: 'Software Engineer, Frontend',
+    location: 'San Francisco, CA',
+    thumb:
+      'https://d2q79iu7y748jz.cloudfront.net/s/_squarelogo/c8614ed0797768bfd1e7fb4dac77f5df',
+    description: {
+      company: `Founded by a team of Facebook, Dropbox, and LinkedIn alumni in 2017, Forethought’s mission is to "enable everyone to be a genius at their job". We're building AI-driven user experiences that embed information into employee workflows, starting with our AI solution for customer support agents: Agatha.
+      Forethought has raised over $10M in VC funding from top-investors including New Enterprise Associates (NEA), Village Global (the venture firm backed by Mark Zuckerberg, Bill Gates, and Reid Hoffman), K9 Ventures, and the CEOs of Robinhood, Front, and Carta. In 2018, Forethought launched and won at TechCrunch Disrupt - the world's most prestigious startup competition - and was featured in Forbes 30 Under 30.
+      We are building the future of unstructured information in the enterprise, tackling a $30B+ market opportunity, and we’re assembling a world-class team to help us get there.`,
+      role: `As a member of the Front-End Team, you will be responsible for developing the user-facing products, features, and platforms at Forethought. You will build features ranging from beautiful search tools for workers, to integrated Zendesk, Slack, or Chrome apps. Our tech stack leverages modern technologies such as React.js, Python/Flask, and MongoDB. As an early member of the team, you will work cross-functionally with our Design, Product, and Back-End teams to build beautiful and elegant products out of complex problems. You will be responsible for architecting the front-end, should know HTML, CSS, and modern JavaScript cold, and be comfortable writing and communicating with REST APIs.`,
+      requirements: `Proficiency in HTML/CSS/JS with modern libraries (React.js, Vue.js, etc.)
+      Strong coding ability; able to work in large code-bases with fluency
+      Solid grasp of computer science, algorithms and data structures
+      Past work building web or mobile applications in a real world setting
+      Detailed ability to implement pixel-perfect UI from pixel-perfect mock-ups
+      "Full-stack" mentality; ability to write backend code in Python or Node.js
+      Adaptable, humble, and interested in making an impact
+      3+ years experience in Front-End or UI Developer job
+      [Required] BSc in Computer Science, or equivalent work experience`,
+      perks: `A fast-paced and collaborative environment
+      Build category-defining products
+      Work with world class talent (ex Facebook, Palantir, Dropbox, LinkedIn)
+      A chance to be an early member of the team, with equity to match
+      Make an impact, and help make everyone a genius at their job
+      Medical, dental and vision coverage
+      Unlimited PTO policy`,
+    },
+  },
+  {
+    id: 2,
+    company: 'Forethought',
+    jobTitle: 'Software Engineer, Frontend',
+    location: 'San Francisco, CA',
+    thumb:
+      'https://d2q79iu7y748jz.cloudfront.net/s/_squarelogo/c8614ed0797768bfd1e7fb4dac77f5df',
+    description: {
+      company: `Founded by a team of Facebook, Dropbox, and LinkedIn alumni in 2017, Forethought’s mission is to "enable everyone to be a genius at their job". We're building AI-driven user experiences that embed information into employee workflows, starting with our AI solution for customer support agents: Agatha.
+      Forethought has raised over $10M in VC funding from top-investors including New Enterprise Associates (NEA), Village Global (the venture firm backed by Mark Zuckerberg, Bill Gates, and Reid Hoffman), K9 Ventures, and the CEOs of Robinhood, Front, and Carta. In 2018, Forethought launched and won at TechCrunch Disrupt - the world's most prestigious startup competition - and was featured in Forbes 30 Under 30.
+      We are building the future of unstructured information in the enterprise, tackling a $30B+ market opportunity, and we’re assembling a world-class team to help us get there.`,
+      role: `As a member of the Front-End Team, you will be responsible for developing the user-facing products, features, and platforms at Forethought. You will build features ranging from beautiful search tools for workers, to integrated Zendesk, Slack, or Chrome apps. Our tech stack leverages modern technologies such as React.js, Python/Flask, and MongoDB. As an early member of the team, you will work cross-functionally with our Design, Product, and Back-End teams to build beautiful and elegant products out of complex problems. You will be responsible for architecting the front-end, should know HTML, CSS, and modern JavaScript cold, and be comfortable writing and communicating with REST APIs.`,
+      requirements: `Proficiency in HTML/CSS/JS with modern libraries (React.js, Vue.js, etc.)
+      Strong coding ability; able to work in large code-bases with fluency
+      Solid grasp of computer science, algorithms and data structures
+      Past work building web or mobile applications in a real world setting
+      Detailed ability to implement pixel-perfect UI from pixel-perfect mock-ups
+      "Full-stack" mentality; ability to write backend code in Python or Node.js
+      Adaptable, humble, and interested in making an impact
+      3+ years experience in Front-End or UI Developer job
+      [Required] BSc in Computer Science, or equivalent work experience`,
+      perks: `A fast-paced and collaborative environment
+      Build category-defining products
+      Work with world class talent (ex Facebook, Palantir, Dropbox, LinkedIn)
+      A chance to be an early member of the team, with equity to match
+      Make an impact, and help make everyone a genius at their job
+      Medical, dental and vision coverage
+      Unlimited PTO policy`,
+    },
+  },
+  {
+    id: 3,
+    company: 'Forethought',
+    jobTitle: 'Software Engineer, Frontend',
+    location: 'San Francisco, CA',
+    thumb:
+      'https://d2q79iu7y748jz.cloudfront.net/s/_squarelogo/c8614ed0797768bfd1e7fb4dac77f5df',
+    description: {
+      company: `Founded by a team of Facebook, Dropbox, and LinkedIn alumni in 2017, Forethought’s mission is to "enable everyone to be a genius at their job". We're building AI-driven user experiences that embed information into employee workflows, starting with our AI solution for customer support agents: Agatha.
+      Forethought has raised over $10M in VC funding from top-investors including New Enterprise Associates (NEA), Village Global (the venture firm backed by Mark Zuckerberg, Bill Gates, and Reid Hoffman), K9 Ventures, and the CEOs of Robinhood, Front, and Carta. In 2018, Forethought launched and won at TechCrunch Disrupt - the world's most prestigious startup competition - and was featured in Forbes 30 Under 30.
+      We are building the future of unstructured information in the enterprise, tackling a $30B+ market opportunity, and we’re assembling a world-class team to help us get there.`,
+      role: `As a member of the Front-End Team, you will be responsible for developing the user-facing products, features, and platforms at Forethought. You will build features ranging from beautiful search tools for workers, to integrated Zendesk, Slack, or Chrome apps. Our tech stack leverages modern technologies such as React.js, Python/Flask, and MongoDB. As an early member of the team, you will work cross-functionally with our Design, Product, and Back-End teams to build beautiful and elegant products out of complex problems. You will be responsible for architecting the front-end, should know HTML, CSS, and modern JavaScript cold, and be comfortable writing and communicating with REST APIs.`,
+      requirements: `Proficiency in HTML/CSS/JS with modern libraries (React.js, Vue.js, etc.)
+      Strong coding ability; able to work in large code-bases with fluency
+      Solid grasp of computer science, algorithms and data structures
+      Past work building web or mobile applications in a real world setting
+      Detailed ability to implement pixel-perfect UI from pixel-perfect mock-ups
+      "Full-stack" mentality; ability to write backend code in Python or Node.js
+      Adaptable, humble, and interested in making an impact
+      3+ years experience in Front-End or UI Developer job
+      [Required] BSc in Computer Science, or equivalent work experience`,
+      perks: `A fast-paced and collaborative environment
+      Build category-defining products
+      Work with world class talent (ex Facebook, Palantir, Dropbox, LinkedIn)
+      A chance to be an early member of the team, with equity to match
+      Make an impact, and help make everyone a genius at their job
+      Medical, dental and vision coverage
+      Unlimited PTO policy`,
+    },
+  },
+  {
+    id: 4,
+    company: 'Forethought',
+    jobTitle: 'Software Engineer, Frontend',
+    location: 'San Francisco, CA',
+    thumb:
+      'https://d2q79iu7y748jz.cloudfront.net/s/_squarelogo/c8614ed0797768bfd1e7fb4dac77f5df',
+    description: {
+      company: `Founded by a team of Facebook, Dropbox, and LinkedIn alumni in 2017, Forethought’s mission is to "enable everyone to be a genius at their job". We're building AI-driven user experiences that embed information into employee workflows, starting with our AI solution for customer support agents: Agatha.
+      Forethought has raised over $10M in VC funding from top-investors including New Enterprise Associates (NEA), Village Global (the venture firm backed by Mark Zuckerberg, Bill Gates, and Reid Hoffman), K9 Ventures, and the CEOs of Robinhood, Front, and Carta. In 2018, Forethought launched and won at TechCrunch Disrupt - the world's most prestigious startup competition - and was featured in Forbes 30 Under 30.
+      We are building the future of unstructured information in the enterprise, tackling a $30B+ market opportunity, and we’re assembling a world-class team to help us get there.`,
+      role: `As a member of the Front-End Team, you will be responsible for developing the user-facing products, features, and platforms at Forethought. You will build features ranging from beautiful search tools for workers, to integrated Zendesk, Slack, or Chrome apps. Our tech stack leverages modern technologies such as React.js, Python/Flask, and MongoDB. As an early member of the team, you will work cross-functionally with our Design, Product, and Back-End teams to build beautiful and elegant products out of complex problems. You will be responsible for architecting the front-end, should know HTML, CSS, and modern JavaScript cold, and be comfortable writing and communicating with REST APIs.`,
+      requirements: `Proficiency in HTML/CSS/JS with modern libraries (React.js, Vue.js, etc.)
+      Strong coding ability; able to work in large code-bases with fluency
+      Solid grasp of computer science, algorithms and data structures
+      Past work building web or mobile applications in a real world setting
+      Detailed ability to implement pixel-perfect UI from pixel-perfect mock-ups
+      "Full-stack" mentality; ability to write backend code in Python or Node.js
+      Adaptable, humble, and interested in making an impact
+      3+ years experience in Front-End or UI Developer job
+      [Required] BSc in Computer Science, or equivalent work experience`,
+      perks: `A fast-paced and collaborative environment
+      Build category-defining products
+      Work with world class talent (ex Facebook, Palantir, Dropbox, LinkedIn)
+      A chance to be an early member of the team, with equity to match
+      Make an impact, and help make everyone a genius at their job
+      Medical, dental and vision coverage
+      Unlimited PTO policy`,
+    },
+  },
+];
