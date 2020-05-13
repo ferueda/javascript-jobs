1ebd3aa869e802f831ce5140cad5af33a6d5dff3
ferueda <fe.rueda@outlook.com>
2020-05-13 12:13:39 -0400

    added tests for the <Search /> component

--- src/components/Search.test.js
+++ src/components/Search.test.js
@@ -1,7 +1,7 @@
 import '@testing-library/jest-dom/extend-expect';
 import 'jest-axe/extend-expect';
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, queryAllByTestId } from '@testing-library/react';
 import user from '@testing-library/user-event';
 import { axe } from 'jest-axe';
 import Search from './Search';
@@ -11,7 +11,9 @@ describe('<Search />', () => {
     const formElement = event.target;
     return formElement.querySelector('input').value;
   });
-  const mockCitySelectionHandler = jest.fn();
+  const mockCitySelectionHandler = jest.fn((event) => {
+    return event.target.value;
+  });
 
   const searchProps = {
     placeholder: 'TEST_PLACEHOLDER',
@@ -20,7 +22,7 @@ describe('<Search />', () => {
     handleCitySelection: mockCitySelectionHandler,
   };
 
-  describe('Form element', () => {
+  describe('form element', () => {
     test('form, input and button elements are rendered', () => {
       const { getByLabelText, getByTestId } = render(<Search {...searchProps} />);
 
@@ -44,7 +46,7 @@ describe('<Search />', () => {
       expect(input.value).toBe(testInputValue);
     });
 
-    test('search handler is called and returns with the right value after submitting form', () => {
+    test('search handler is called and returns the right value after submitting form', () => {
       const testInputValue = 'TEST';
 
       const { getByLabelText, getByTestId } = render(<Search {...searchProps} />);
@@ -75,6 +77,39 @@ describe('<Search />', () => {
 
       expect(select).toBeInTheDocument();
       expect(options.length).toBeGreaterThan(0);
+      expect(options).toMatchInlineSnapshot(`
+        Array [
+          <option
+            value="sydney"
+          >
+            Sydney
+          </option>,
+          <option
+            value="melbourne"
+          >
+            Melbourne
+          </option>,
+        ]
+      `);
+    });
+
+    test('on change, handler func is called and returs the right value', () => {
+      const { getByLabelText, queryAllByRole } = render(<Search {...searchProps} />);
+
+      const select = getByLabelText(/select city/i);
+      const options = queryAllByRole('option');
+
+      fireEvent.change(select, { target: { value: options[0].value } });
+
+      expect(mockCitySelectionHandler).toHaveBeenCalledTimes(1);
+      expect(mockCitySelectionHandler).toHaveReturnedTimes(1);
+      expect(mockCitySelectionHandler).toHaveReturnedWith(options[0].value);
+
+      fireEvent.change(select, { target: { value: options[1].value } });
+
+      expect(mockCitySelectionHandler).toHaveBeenCalledTimes(2);
+      expect(mockCitySelectionHandler).toHaveReturnedTimes(2);
+      expect(mockCitySelectionHandler).toHaveReturnedWith(options[1].value);
     });
   });
 });
