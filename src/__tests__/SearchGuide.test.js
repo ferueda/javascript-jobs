73f646083ababe577588c486069f53d18f14a9ee
ferueda <fe.rueda@outlook.com>
2020-05-14 22:02:27 -0400

    <SearchGuide tests initial commit

---
+++ src/__tests__/SearchGuide.test.js
@@ -0,0 +1,9 @@
+import '@testing-library/jest-dom/extend-expect';
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import user from '@testing-library/user-event';
+import SearchGuide from '../components/SearchGuide';
+
+describe('<SearchGuide />', () => {
+  test('it renders', () => {});
+});
