17dde26ff2e9c3db99e9b260f8ecec687db2540a
ferueda <fe.rueda@outlook.com>
2020-05-14 20:11:20 -0400

    added tests for the <Nav /> component

--- src/__tests__/Nav.test.js
+++ src/__tests__/Nav.test.js
@@ -1,28 +1,51 @@
 import '@testing-library/jest-dom/extend-expect';
+import user from '@testing-library/user-event';
 import React from 'react';
-import { render, getAllByLabelText } from '@testing-library/react';
+import { render, fireEvent, wait, cleanup } from '@testing-library/react';
 import Nav from '../components/Nav';
 import { techFilters } from '../utils/helpers';
 
 describe('<Nav />', () => {
   const filterTest = [];
+  const mockedHandleFilters = jest.fn();
 
   const navProps = {
-    handleFilter: jest.fn(),
+    handleFilters: mockedHandleFilters,
     filter: filterTest,
   };
 
-  test('it renders', () => {
-    const { getByRole, getAllByRole, debug } = render(<Nav {...navProps} />);
+  const renderComponent = () => {
+    const utils = render(<Nav {...navProps} />);
 
-    const nav = getByRole('navigation');
-    const techLogos = getAllByRole('img');
+    const nav = utils.getByRole('navigation');
+    const techLogos = utils.getAllByRole('img');
     const techDescp = nav.querySelectorAll('span');
+
+    return {
+      ...utils,
+      nav,
+      techLogos,
+      techDescp,
+    };
+  };
+
+  test('it renders', async () => {
+    const { nav, techLogos, techDescp } = renderComponent();
 
     expect(nav).toBeInTheDocument();
     expect(techLogos.length).toBe(techFilters.length);
     expect(techDescp.length).toBe(techFilters.length);
+  });
 
-    debug();
+  test('on click, handler is called', async () => {
+    const { techDescp, debug } = renderComponent();
+    const clickedLogo = techDescp[2];
+
+    await wait(() => user.click(clickedLogo));
+
+    expect(mockedHandleFilters).toHaveBeenCalledTimes(1);
+    expect(mockedHandleFilters).toHaveBeenCalledWith(
+      clickedLogo.textContent.toLowerCase()
+    );
   });
 });
