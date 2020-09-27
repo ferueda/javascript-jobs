a25e7dac7708162cd3db1ec4327634d1bcc9abc0
ferueda <fe.rueda@outlook.com>
2020-09-27 18:52:12 -0300

    Refactor glovalStyles.js
    Extracted global styles into its own file

---
+++ src/styles/globalStyles.js
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    font-family: 'IBM Plex Sans', sans-serif;
+    box-sizing: border-box;
+    font-size: 10px;
+  }
+
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+
+  body, h1, h2, h3, h4, h5, h6, p, ol, ul {
+    margin: 0;
+    padding: 0;
+    font-weight: normal;
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+  }
+`;
+
+export default GlobalStyle;
